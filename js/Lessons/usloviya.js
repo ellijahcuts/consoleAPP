c9570296aabc7a130dcf605785e5f94fa40d962b
ellijahcuts <ellijahcuts@gmail.com>
2022-01-11 14:54:07 +0300

    2. We enter data into the database using loops. For, while and do while and use the conditions

---
+++ js/Lessons/usloviya.js
@@ -0,0 +1,35 @@
+"use strict"
+
+if (1 === 9) {
+    console.log("Ok!")
+} else {
+    console.log("Nihua")
+}
+
+
+
+if (num < 9){
+    console.log("Oshibka")
+}else if(num > 11){
+    console.log("Toje")
+}else {
+    console.log("ok")}
+
+
+(num === 10) ? console.log("Pushka") : console.log("Net")
+
+const num = 10
+switch (num) {
+    case 49:
+        console.log("net")
+        break
+    case 9:
+        console.log("nea")
+        break
+    case 10:
+        console.log("krasava")
+        break
+    default:
+        console.log("Nirazy ne popal")
+        break
+}
--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
 "use strict"
 
-
-let firstFilmName = prompt("Один из последних просмотренных фильмов", "Название")
-let firstFilmScore = +prompt("Какую оценку вы бы поставили этому фильму?", "от 1 до 10")
-let secondFilmName = prompt("Еще один из последних просмотренных фильмов", "Название")
-let secondFilmScore = +prompt("Какую оценку вы бы поставили этому фильму?", "от 1 до 10")
-
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "Ваш ответ")
-console.log(numberOfFilms)
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -16,7 +9,58 @@ const personalMovieDB = {
     genres: [],
     privat: false
 }
-personalMovieDB.movies[firstFilmName] = firstFilmScore
-personalMovieDB.movies[secondFilmName] = secondFilmScore
-console.log(personalMovieDB)
+
+for (let i = 0; i < 2; i++) {
+    let filmName = prompt("Как называется фильм который вы смотрели?", ""),
+        filmScore = prompt("Какую бы оценку вы ему поставили", "")
+
+    if (filmName != null && filmScore != null && filmName !== "" && filmScore !== "" && filmName.length < 50) {
+        personalMovieDB.movies[filmName] = filmScore
+    } else {
+        i--
+    }
+}
+
+//Решение do while
+/*let i = 0
+do {
+    let filmName = prompt("Как называется фильм который вы смотрели?", ""),
+        filmScore = prompt("Какую бы оценку вы ему поставили", "")
+    i++
+    if (filmName != null && filmScore != null && filmName !== "" && filmScore !== "" && filmName.length < 50) {
+        console.log('done')
+        personalMovieDB.movies[filmName] = filmScore
+    } else {
+        console.log('error')
+        i--
+    }
+} while (i < 2)*/
+
+//Решение while
+/*let i = 0
+while (i < 2) {
+    let filmName = prompt("Как называется фильм который вы смотрели?", ""),
+        filmScore = prompt("Какую бы оценку вы ему поставили", "")
+    i++
+    if (filmName != null && filmScore != null && filmName !== "" && filmScore !== "" && filmName.length < 50) {
+        console.log('done')
+        personalMovieDB.movies[filmName] = filmScore
+    } else {
+        console.log('error')
+        i--
+    }
+}*/
+
+if (personalMovieDB.count < 10) {
+    console.log("Да вы братец мало фильмов смотрите")
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Да вы милок обычный посетитель кинозала")
+} else if (personalMovieDB.count >= 30) {
+    console.log("Мисье да вы киноман")
+} else {
+    console.log("Ошибочка вышла")
+}
+
+
+console.log(personalMovieDB.movies)
 
